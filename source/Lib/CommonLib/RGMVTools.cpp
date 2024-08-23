1a567df90b0562c13d08e2a601063de9fe39642f
Hope <1554061588@qq.com>
2024-08-23 10:26:07 +0800

    RGMV Tools
    RGMV Tools：
    Read the RGMV_Piexl_Error
    and decide the RGMV Confience

--- source/Lib/CommonLib/RGMVTools.cpp
+++ source/Lib/CommonLib/RGMVTools.cpp
@@ -8,6 +8,11 @@ void allocate_YUV444_buffers(YUVBuffer_16bit &buffer, int width, int height)
   buffer.vBuffer = std::make_unique<uint16_t[]>(width * height * 2);
 }
 
+void allocate_Y_buffers(YBuffer_8bit &buffer, int width, int height)
+{
+  buffer.yBuffer = std::make_unique<uint8_t[]>(width * height);
+}
+
 void read_YUV444_frame(const std::string &yuvFilePath, int frameIndex, int width, int height, YUVBuffer_16bit &buffer)
 {
   std::ifstream file(yuvFilePath, std::ios::binary);
@@ -27,6 +32,25 @@ void read_YUV444_frame(const std::string &yuvFilePath, int frameIndex, int width
   file.read(reinterpret_cast<char *>(buffer.vBuffer.get()), width * height * 2);
   file.close();
 }
+
+void read_Y_8bit_frame(const std::string &yuvFilePath, int frameIndex, int width, int height, YBuffer_8bit &buffer)
+{
+  std::ifstream file(yuvFilePath, std::ios::binary);
+  if (!file.is_open())
+  {
+    throw std::runtime_error("无法打开YUV文件");
+  }
+
+  file.seekg(frameIndex * width * height * 6, std::ios::beg);
+
+  // 分配内存
+  allocate_Y_buffers(buffer, width, height);
+
+  // 读取帧数据
+  file.read(reinterpret_cast<char *>(buffer.yBuffer.get()), width * height * 2);
+  file.close();
+}
+
 bool validate_coordinate(int PosX, int PosY, int width, int height)
 {
   if (PosX >= 0 && PosX < width && PosY >= 0 && PosY < height)
@@ -73,4 +97,40 @@ std::array<float, 2> get_piexl_RGMV(YUVBuffer_16bit &buffer, int PosX, int PosY,
   float ScreenMv_V = -1.0f * Accuracy_corr * (((float(DepthMv_UINT16[2]) - 32768.0f) * 2.0f) / 65536.0f) * (height - 1);
   std::array<float, 2> RGMV = { ScreenMv_U, ScreenMv_V };
   return RGMV;
+}
+// 访问RG-MV置信度
+uint8_t get_pixel_Y(YBuffer_8bit &buffer, int PosX, int PosY, int width, int height)
+{
+  int     x = PosX < 0 ? 0 : PosX > width - 1 ? width - 1 : PosX;
+  int     y = PosY < 0 ? 0 : PosY > height - 1 ? height - 1 : PosY;
+  uint8_t Y = buffer.yBuffer.get()[y * width + x];
+  return Y;
+}
+
+void example()
+{
+  int frameindex = 1;
+  int width      = 1024;
+  int height     = 1024;
+  // RGMV yuv file path
+  std::string     RGMVFile = "depthmv444_3.yuv";
+  YUVBuffer_16bit RGMVBuffer;
+  read_YUV444_frame(RGMVFile, frameindex, width, height, RGMVBuffer);
+
+  std::string  RGMVConfidenceFile = "RGMVConfidence.yuv";
+  YBuffer_8bit RGMVConfidenceBuffer;
+  read_Y_8bit_frame(RGMVConfidenceFile, frameindex, width, height, RGMVConfidenceBuffer);
+
+  //
+
+  for (int row = 0; row < height; row++)
+  {
+    for (int col = 0; col < width; col++)
+    {
+      std::array<float, 2> RGMV             = get_piexl_RGMV(RGMVBuffer, col, row, width, height);
+      uint8_t              RGMV_Pixel_Error = get_pixel_Y(RGMVConfidenceBuffer, col, row, width, height);
+      float                RGMVConfidence =
+        RGMV_Pixel_Error > 39 ? PixelRGMVPossConfidence[39] : PixelRGMVPossConfidence[int(RGMV_Pixel_Error)];
+    }
+  }
 }
--- source/Lib/CommonLib/RGMVTools.h
+++ source/Lib/CommonLib/RGMVTools.h
@@ -9,19 +9,71 @@
 
 #define Accuracy_corr 16.0 / 15.9687500
 
+const float PixelRGMVPossConfidence[40] = { 1.0,
+                                            0.9999804223185569f,
+                                            0.9998896138226733f,
+                                            0.999404309252218f,
+                                            0.9982471516213848f,
+                                            0.9956574185765983f,
+                                            0.9914913731978255f,
+                                            0.982863233847163f,
+                                            0.9727412618157837f,
+                                            0.9532358476907485f,
+                                            0.9300712068650722f,
+                                            0.893828798938288f,
+                                            0.8606706524564596f,
+                                            0.808649289099526f,
+                                            0.756701030927835f,
+                                            0.684618222058281f,
+                                            0.6179264766625362f,
+                                            0.5518266779949023f,
+                                            0.4768747193533902f,
+                                            0.4125457875457875f,
+                                            0.35260663507109f,
+                                            0.2977346278317152f,
+                                            0.2558674643350207f,
+                                            0.20448430493273542f,
+                                            0.16892502258355918f,
+                                            0.1404710920770878f,
+                                            0.11895754518705338f,
+                                            0.09240658899156287f,
+                                            0.07889467360833f,
+                                            0.06424792139077853f,
+                                            0.05333333333333334f,
+                                            0.04337265787647467f,
+                                            0.03545611015490534f,
+                                            0.027281584930172135f,
+                                            0.02311589613679544f,
+                                            0.019085125719478947f,
+                                            0.015615792575132587f,
+                                            0.013191131069323604f,
+                                            0.010351402887496594f,
+                                            0.008937960042060988f };
+
 struct YUVBuffer_16bit
 {
   std::unique_ptr<uint16_t[]> yBuffer;
   std::unique_ptr<uint16_t[]> uBuffer;
   std::unique_ptr<uint16_t[]> vBuffer;
 };
+struct YBuffer_8bit
+{
+  std::unique_ptr<uint8_t[]> yBuffer;
+};
+
 //分配内存
 void allocate_YUV444_buffers(YUVBuffer_16bit &buffer, int width, int height);
-//读取数据
+void allocate_Y_buffers(YBuffer_8bit &buffer, int width, int height);
+//读取RGMV数据
 void read_YUV444_frame(const std::string &yuvFilePath, int frameIndex, int width, int height, YUVBuffer_16bit &buffer);
+//读取RGMV置信度
+void read_Y_8bit_frame(const std::string &yuvFilePath, int frameIndex, int width, int height, YBuffer_8bit &buffer);
 //坐标是否有效
 bool validate_coordinate(int PosX, int PosY, int width, int height);
 // 访问16bit的数据
 std::array<uint16_t, 3> get_pixel_YUV_16bit(YUVBuffer_16bit &buffer, int PosX, int PosY, int width, int height);
 //访问数据
 std::array<float, 2> get_piexl_RGMV(YUVBuffer_16bit &buffer, int PosX, int PosY, int width, int height);
+
+//访问RGMV 帧间预测置信度
+uint8_t get_pixel_Y(YBuffer_8bit &buffer, int PosX, int PosY, int width, int height);
